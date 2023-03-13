9e26f5a320afea84bc647a397f188ed03f449d29
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-03-13 15:03:52 +0900

    ✨create linked_list_append

---
+++ linked_list/01_linked_list_append.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def print_all(self):
+      cur = self.head
+      while cur is not None:
+          print(cur.data)
+          cur = cur.next
+
+linked_list = LinkedList(5)
+linked_list.append(12)
+linked_list.print_all()
