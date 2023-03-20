0fc717d53514d7de05fdb7d845316b1a04411a8c
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-03-20 13:15:11 +0900

    06_get_kth_node_from_last.py

---
+++ linked_list/06_get_kth_node_from_last.py
@@ -0,0 +1,12 @@
+
+class Node():
+  def __init__(self, data):
+    data = self.data
+    next_data = None
+    
+class LinkedList():
+  def __init__(self, data):
+    head = Node.data(data)
+  
+  def get_kth_node_from_last():
+    retnr
