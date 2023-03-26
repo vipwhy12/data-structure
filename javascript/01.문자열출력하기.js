6e1212d4ec89458a29a3207cf3db813bda8ef818
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-03-27 02:50:56 +0900

    [feat] : 1. 문자열 출력하기✨

---
+++ javascript/01.문자열출력하기.js
@@ -0,0 +1,18 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=javascript
+
+//01. 문자열 출력하기 : 수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log("a = " + input[0]);
+  console.log("b = " + input[1]);
+});
