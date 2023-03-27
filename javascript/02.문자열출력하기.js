0a9ddd8f9a16ca2aaa86284927b23d10e130a85c
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-03-28 02:57:13 +0900

    [feat] : 02.문자열 출력하기✨

---
+++ javascript/02.문자열출력하기.js
@@ -0,0 +1,17 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=javascript
+
+//02. 문자열 출력하기
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+  console.log(str);
+});
