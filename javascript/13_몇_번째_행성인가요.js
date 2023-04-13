94bd650a53a1dcd707968df8e43da4b48d9260a9
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-04-14 01:21:01 +0900

    [fix] : 13. 몇 번째 행성인가요?🐛

--- javascript/13_몇_번째_행성인가요.js
+++ javascript/13_몇_번째_행성인가요.js
@@ -1,19 +1,21 @@
 //13. 몇 번째 행성인가요?
-
-const solarSystem = [
-  "수성",
-  "금성",
-  "지구",
-  "화성",
-  "목성",
-  "토성",
-  "천왕성",
-  "해왕성",
+const planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
 ];
-const input = 1;
 
-for (index in solarSystem) {
-  if (input == index + 1) {
-    console.log(solarSystem[index]);
-  }
-}
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.question("Which planet is it? ", (planet) => {
+  console.log(planets[planet - 1]);
+});
